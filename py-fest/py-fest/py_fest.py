64a2e96cdf8fab03a2d988f1cdedbaf748682a97
juju <jigon220@learner.dvusd.org>
2026-01-12 13:57:57 -0700

    first version 2

--- py-fest/py-fest/py_fest.py
+++ py-fest/py-fest/py_fest.py
@@ -1,1 +1,11 @@
+from random import choice
 
+
+print("\n---Py-Fest 2026 stage manager ---")
+print("1. view linup & total time")
+print("2. add a new band")   
+print("3. move first band to end (late arrival)")  
+print("4. remove a band by name")
+print("5. move band to specific position") # new feature!
+print("6. exit")
+choice = input("select an option (1-6): ")
